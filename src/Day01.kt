61d0f1170c4497cbbc401d5946d6101a27377798
David Pacheco <dapacheco+github@gmail.com>
2021-12-08 21:16:10 +0000

    Added solutions for Day1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,34 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increased = 0
+        input.map { it.toInt() }.fold(0) { acc, i ->
+            if (acc != 0 && i > acc) {
+                increased++
+            }
+            i
+        }
+
+        return increased
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val windowedDepths = List(input.size) { index ->
+            input.drop(index).take(3).sumOf { it.toInt() }
+        }
+        var increased = 0
+        windowedDepths.fold(0) { acc, i ->
+            if (acc != 0 && i > acc) {
+                increased++
+            }
+            i
+        }
+        return increased
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
